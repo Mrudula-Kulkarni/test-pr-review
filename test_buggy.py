6b835bdace12151b3ae587dc70867f7047a446e3
Mrudula-Kulkarni <mrudulakulkarni2001@gmail.com>
2026-04-13 10:58:45 -0700

    update utility functions

--- test_buggy.py
+++ test_buggy.py
@@ -1,3 +1,7 @@
 def calculate_average(numbers):
     total = sum(numbers)
     return total / len(numbers)  # crashes if numbers is empty
+
+def get_user(conn, username):
+    query = "SELECT * FROM users WHERE username = '" + username + "'"
+    return conn.execute(query)  # SQL injection
